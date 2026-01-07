1599af5eb22f5d8a32b68cb14482562a2f97eb9d
Damien <damienostler1@outlook.com>
2026-01-06 22:41:11 -0500

    f

--- backend/src/MechanicBuddy.Management.Api/Infrastructure/KubernetesClient.cs
+++ backend/src/MechanicBuddy.Management.Api/Infrastructure/KubernetesClient.cs
@@ -223,7 +223,8 @@ public class KubernetesClient : IKubernetesClient
                 ["SERVER_SECRET"] = _consumerSecret,
                 ["SESSION_SECRET"] = Convert.ToBase64String([.. Guid.NewGuid().ToByteArray(), .. Guid.NewGuid().ToByteArray()]),
                 ["API_URL"] = $"http://{apiServiceName}:80",
-                ["NEXT_PUBLIC_API_URL"] = $"https://{tenantDomain}/api"
+                ["NEXT_PUBLIC_API_URL"] = $"https://{tenantDomain}/api",
+                ["NEXT_PUBLIC_SESSION_TIMEOUT"] = "3600"
             }
         };
 
